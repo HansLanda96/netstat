fb62c9539f3dfc8a266b96d5e8e63d6f62736985
hanslanda <hanslanda@ubuntu22.04>
2023-08-19 20:26:21 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import subprocess
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/unix_sockets')
+def unix_sockets():
+    result = subprocess.run(['netstat', '-x'], capture_output=True, text=True)
+    return render_template('result.html', title='Unix Sockets', output=result.stdout)
+
+
+@app.route('/tcp_sockets')
+def tcp_sockets():
+    result = subprocess.run(['netstat', '-ant'],
+                            capture_output=True, text=True)
+    return render_template('result.html', title='TCP Sockets', output=result.stdout)
+
+
+@app.route('/routing_table')
+def routing_table():
+    result = subprocess.run(['netstat', '-r'], capture_output=True, text=True)
+    return render_template('result.html', title='Routing Table', output=result.stdout)
+
+
+if __name__ == '__main__':
+    app.run()
